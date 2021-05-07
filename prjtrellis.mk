fdbc392cdeb891d68b51d1f42491aae0eaa4db6a
Alexei A. Smekalkine <ikle@ikle.ru>
2021-05-08 02:28:56 +0300

    Add rules to build prjtrellis

---
+++ prjtrellis.mk
@@ -0,0 +1,43 @@
+#
+# Deps:	python3-dev libboost-filesystem-dev libboost-thread-dev
+#	libboost-program-options-dev
+#
+
+PREFIX	?= "$(HOME)/cad"
+
+MODULE	= prjtrellis
+
+SRC	= $(CURDIR)/$(MODULE)/libtrellis
+WORK	= $(CURDIR)/$(MODULE)-build
+PACK	= $(CURDIR)/$(MODULE)-pack
+
+TARGET	= $(MODULE)-pack.tgz
+
+.PHONY: all fetch configure compile install package clean
+
+all: package
+
+fetch:
+	git submodule init
+	git submodule update --init --recursive $(MODULE)
+
+configure: fetch
+	rm -rf "$(WORK)"
+	mkdir "$(WORK)"
+	cd "$(WORK)" && \
+	cmake "$(SRC)" \
+		-DCMAKE_INSTALL_PREFIX="$(PREFIX)" \
+		-DCMAKE_BUILD_TYPE=Release
+
+compile: configure
+	make -C "$(WORK)" -j4
+
+install: compile
+	rm -rf "$(PACK)"
+	umask 022 && make -C "$(WORK)" DESTDIR="$(PACK)" install
+
+package: install
+	fakeroot tar czf "$(TARGET)" -C "$(PACK)" .
+
+clean:
+	rm -rf "$(TARGET)" "$(PACK)" "$(WORK)"
