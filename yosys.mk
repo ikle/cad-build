15fe06fef5d7454a0523b7d4ed4b27b3a16e17a7
Alexei A. Smekalkine <ikle@ikle.ru>
2021-05-12 03:25:28 +0300

    yosys: use common rules, simplify

--- yosys.mk
+++ yosys.mk
@@ -4,41 +4,18 @@
 #	libboost-python-dev libboost-filesystem-dev zlib1g-dev
 #
 
-PREFIX	?= "$(HOME)/cad"
-
 MODULE	= yosys
 
-SRC	= $(CURDIR)/$(MODULE)
-WORK	= $(CURDIR)/$(MODULE)-build
-PACK	= $(CURDIR)/$(MODULE)-pack
+include make-module.mk
 
-TARGET	= $(MODULE)-pack.tgz
-
-.PHONY: all fetch configure compile install package clean
-
-all: package
-
-fetch:
-	git submodule init
-	git submodule update --init --recursive $(MODULE)
-
-configure: fetch
-	rm -rf "$(WORK)"
-	mkdir "$(WORK)"
+configure:
 	cd "$(WORK)" && \
 	make -f "$(SRC)/Makefile" config-gcc
 
-compile: configure
+compile:
 	make -f "$(SRC)/Makefile" -C "$(WORK)" PREFIX="$(PREFIX)" -j4
 
-install: compile
-	rm -rf "$(PACK)"
+install:
 	umask 022 && \
 	make -f "$(SRC)/Makefile" -C "$(WORK)" \
 	     PREFIX="$(PREFIX)" DESTDIR="$(PACK)" install
-
-package: install
-	fakeroot tar czf "$(TARGET)" -C "$(PACK)" .
-
-clean:
-	rm -rf "$(TARGET)" "$(PACK)" "$(WORK)"
